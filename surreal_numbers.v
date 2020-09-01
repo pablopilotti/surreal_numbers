e643c6ecf81d84b1d08a789d20fe1d4086464741
Francisco Ferreira <fferreira@users.noreply.github.com>
2020-09-01 21:59:18 +0100

    made some cosmetic changes and added some structure to the big proof (#1)

--- surreal_numbers.v
+++ surreal_numbers.v
@@ -1,11 +1,8 @@
 Require Import List.
+Import ListNotations.
 Require Import Classical.
 Require Import Classical_Prop.
 Require Import Init.Logic.
-Notation "x :: l" := (cons x l)
-                     (at level 60, right associativity).
-Notation "[ ]" := nil.
-Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
 
 
 (* Definion de Simbolos *)
@@ -13,33 +10,29 @@ Inductive symbol: Type :=
 | pair: list symbol -> list symbol -> symbol.
 Notation "( x , y )" := (pair x y).
 
-Fixpoint left (s : symbol) : list symbol :=
+Definition left (s : symbol) : list symbol :=
 match s with
 | pair l _ => l
 end.
 
-Fixpoint right (s : symbol) : list symbol :=
+Definition right (s : symbol) : list symbol :=
 match s with
 | pair _ r => r
 end.
-Hint Unfold left right.
 
 (* Ejemplo de Simbolos *)
 Definition n0 := ([],[]).
 Definition n_1 := ([],[n0]).
 Definition n1 := ([n0],[]).
-Hint Unfold n0 n1 n_1.
 
 (* Definicion de: No es mayor o igual *)
 Axiom ngeq: list symbol-> list symbol -> Prop.
 Axiom forall_ngeq_r: forall (X Y: list symbol), ngeq X Y <-> forall (y: symbol), In y Y -> ngeq X [y].
 Axiom forall_ngeq_l: forall (X Y: list symbol), ngeq X Y <-> forall (x: symbol), In x X -> ngeq [x] Y.
-Hint Resolve forall_ngeq_l forall_ngeq_r.
 
 (* Definicion de: Es menor o igual *)
 Axiom leq: symbol-> symbol -> Prop.
 Axiom leq_def: forall (X: symbol), forall (Y: symbol), (ngeq (left X) [Y] /\ ngeq [X] (right Y)) <-> leq X Y.
-Hint Resolve leq_def.
 
 (* TODO mejorar la notacion *)
 Notation "x _<=_ y" := (leq x y)
@@ -50,16 +43,20 @@ Notation "~ ( x _>=_  y )" := (ngeq x y)
 (* Definicion de: Es menor o igual *)
 Axiom leq_n: forall (X: symbol), forall (Y: symbol), ngeq [Y] [X] <-> (leq X Y -> False).
 Axiom ngeq_n: forall (X: symbol), forall (Y: symbol), leq X Y <-> (ngeq [Y] [X] -> False).
-Hint Resolve leq_n ngeq_n.
 
 Check ngeq_n.
 
 Definition is_number (x: symbol) := ngeq (left x) (right x).
-Hint Unfold is_number.
 
 (*Pruebas de que 0, 1 y -1 son numeros y 0 <= 0, *)
-Lemma Zero_is_number: (is_number n0). Proof. unfold is_number. unfold n0. unfold left. apply forall_ngeq_l. intros. tauto. Qed.
-Lemma One_is_number: (is_number n1).  Proof. unfold is_number. unfold n1. unfold n0. unfold right. apply forall_ngeq_r. intros. tauto. Qed.
+Lemma Zero_is_number: (is_number n0).
+Proof.
+  (* unfold is_number. unfold n0. unfold left.*)
+  apply forall_ngeq_l. intros. tauto.
+Qed.
+
+
+Lemma One_is_number: (is_number n1).  Proof. (* unfold is_number. unfold n1. unfold n0. unfold right. *) apply forall_ngeq_r. intros. tauto. Qed.
 Lemma minusOne_is_number: (is_number n_1). Proof. unfold is_number. unfold n_1. unfold n0. unfold left. apply forall_ngeq_l. intros. tauto. Qed.
 Lemma leq_Zero_Zero: leq n0 n0. Proof. unfold n0. apply leq_def. split. unfold left. apply forall_ngeq_l. intros. tauto. unfold right. apply forall_ngeq_r. intros. tauto.  Qed.
 Lemma leq_minusOne_Zero: leq n_1 n0. Proof. unfold n_1. unfold n0. apply leq_def. split. unfold left. apply forall_ngeq_l. intros. tauto. unfold right. apply forall_ngeq_r. intros. tauto.  Qed.
@@ -75,11 +72,7 @@ Definition bad_number(X Y Z: symbol):= leq X Y /\ leq Y Z /\ ~leq X Z.
 Lemma not_leq_to_or: forall (X Y: symbol), (leq X Y -> False) -> ((ngeq (left X) [Y]-> False) \/ (ngeq [X] (right Y) ->False)).
 Proof.
 intros.
-elim (classic ((ngeq (left X) [Y] /\ ngeq [X] (right Y)))).
-intros.
-apply leq_def in H0.
-tauto.
-tauto.
+elim (classic ((ngeq (left X) [Y] /\ ngeq [X] (right Y)))) ; [ intros ; apply leq_def in H0 |] ; tauto.
 Qed.
 
 (* No encuentro este teorema en mi libreria *)
@@ -97,8 +90,8 @@ apply forall_ngeq_l.
 Qed.
 
 
-Lemma not_forall_exists_not_l: forall (x Y: symbol),forall (X: list symbol), 
-             ((~ forall x : symbol, In x X -> ngeq [x] [Y])) -> 
+Lemma not_forall_exists_not_l: forall (x Y: symbol),forall (X: list symbol),
+             ((~ forall x : symbol, In x X -> ngeq [x] [Y])) ->
              ((exists x : symbol, In x X -> (ngeq [x] [Y]->False))).
 Proof.
 intros.
@@ -118,8 +111,8 @@ apply forall_ngeq_r.
 Qed.
 
 
-Lemma not_forall_exists_not_r: forall (y X: symbol),forall (Y: list symbol), 
-             ((~ forall y : symbol, In y Y -> ngeq [X] [y])) -> 
+Lemma not_forall_exists_not_r: forall (y X: symbol),forall (Y: list symbol),
+             ((~ forall y : symbol, In y Y -> ngeq [X] [y])) ->
                ((exists y : symbol, In y Y -> (ngeq [X] [y]->False))).
 Proof.
 intros.
@@ -132,66 +125,64 @@ tauto.
 Qed.
 (* ----------------- Lemas auxiliares -----------------*)
 
-Lemma bad_numbers: forall (X Y Z: symbol), bad_number X Y Z -> 
-               (exists x : symbol, In x (left X) -> bad_number Y Z x) \/                                                            
+Lemma bad_numbers: forall (X Y Z: symbol), bad_number X Y Z ->
+               (exists x : symbol, In x (left X) -> bad_number Y Z x) \/
                (exists z : symbol, In z (right Z) ->bad_number z X Y).
 
 Proof.
-unfold bad_number.
-intros.
-destruct H.
-destruct H0.
-apply not_leq_to_or in H1.
-destruct H1.
-left.
-apply leq_def in H.
-apply leq_def in H0.
-destruct H.
-destruct H0.
-apply not_forall_ngeq_l in H1.
-apply not_forall_exists_not_l in H1.
-elim H1.
-intros.
-exists x.
-intros.
-split. 
-apply leq_def.
-split. eauto.
-eauto.
-split.
-apply ngeq_n.
-eauto.
-unfold not.
-apply leq_n.
-rewrite (forall_ngeq_l (left X) [Y]) in H.
-eauto.
-eauto.
-eauto.
-
-right.
-apply leq_def in H.
-apply leq_def in H0.
-destruct H.
-destruct H0.
-apply not_forall_ngeq_r in H1.
-apply not_forall_exists_not_r in H1.
-elim H1.
-intros z.
-intros.
-exists z.
-intros.
-split. 
-apply ngeq_n.
-eauto.
-split.
-apply leq_def.
-eauto.
-unfold not.
-apply leq_n.
-rewrite (forall_ngeq_r [Y] (right Z)) in H3.
-eauto.
-eauto.
-eauto.
+  unfold bad_number.
+  intros.
+  destruct H.
+  destruct H0.
+  apply not_leq_to_or in H1.
+  destruct H1.
+  {
+    left.
+    apply leq_def in H.
+    apply leq_def in H0.
+    destruct H.
+    destruct H0.
+    apply not_forall_ngeq_l in H1 ; last easy.
+    apply not_forall_exists_not_l in H1 ; last easy.
+    elim H1.
+    {
+      intros.
+      exists x.
+      intros.
+      split.
+      {
+        apply leq_def.
+        split;  eauto.
+      }
+      {
+        split ; first  (apply ngeq_n ; eauto).
+        unfold not.
+        apply leq_n.
+        rewrite (forall_ngeq_l (left X) [Y]) in H.
+        eauto.
+      }
+    }
+  }
+  {
+    right.
+    apply leq_def in H.
+    apply leq_def in H0.
+    destruct H.
+    destruct H0.
+    apply not_forall_ngeq_r in H1 ; last easy.
+    apply not_forall_exists_not_r in H1 ; last easy.
+    elim H1.
+    intros z.
+    intros.
+    exists z.
+    intros.
+    split ; first (apply ngeq_n ; eauto).
+    split ; first (apply leq_def ; eauto).
+    unfold not.
+    apply leq_n.
+    rewrite (forall_ngeq_r [Y] (right Z)) in H3.
+    eauto.
+  }
 Qed.
 
 
@@ -203,14 +194,10 @@ Fixpoint count_s (s:symbol) : nat :=
                     | s::l => max (count_s s) (count_l l)
                   end) in
   match s with
-  | pair x y  => 1 + max (count_l x) (count_l y) 
+  | pair x y  => 1 + max (count_l x) (count_l y)
   end.
 
-Lemma p : count_s n1 = count_s n_1. 
-Proof. 
-unfold n_1, n1, n0. unfold count_s. eauto. 
+Lemma p : count_s n1 = count_s n_1.
+Proof.
+unfold n_1, n1, n0. unfold count_s. eauto.
 Qed.
-
-
-
-
