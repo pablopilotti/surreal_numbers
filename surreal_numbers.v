df1d91ae37bea96737cc0985fa09d00d121b9224
Pilotti, Pablo Emiliano <ppilotti@irobot.com>
2020-09-12 20:47:47 +0100

    all proofs done, until chapter 4.

--- surreal_numbers.v
+++ surreal_numbers.v
@@ -8,6 +8,8 @@ Require Import Arith.
 Require Import Arith.Max.
 Require Import Coq.micromega.Lia.
 Require Import Coq.Structures.GenericMinMax.
+Require Import Coq.Program.Tactics.
+Require Import Coq.Program.Equality.
 
 (* Definion de Simbolos *)
 Inductive symbol: Type :=
@@ -68,6 +70,35 @@ Axiom forall_ngeq_l: forall (X Y: list symbol), ngeq X Y <-> forall (x: symbol),
 (* Definicion de: Es menor o igual *)
 Axiom leq: symbol-> symbol -> Prop.
 Axiom leq_def: forall (X: symbol), forall (Y: symbol), (ngeq (left X) [Y] /\ ngeq [X] (right Y)) <-> leq X Y.
+
+
+Lemma ngeq_l: forall (X Y: list symbol) (x: symbol), ngeq X Y /\ ngeq [x] Y <-> ngeq (x::X) Y.
+Proof.
+split.
+{ 
+intros; destruct H; apply forall_ngeq_l;intros;elim H1.
+intros;rewrite <-H2;tauto.
+apply forall_ngeq_l;tauto.
+} {
+intros;rewrite (forall_ngeq_l (x :: X) Y) in H;split.
+rewrite (forall_ngeq_l X Y);intros;apply H;apply in_cons;eauto.
+apply H;apply in_eq.
+}
+Qed.
+
+Lemma ngeq_r: forall (X Y: list symbol) (y: symbol), ngeq X Y /\ ngeq X [y] <-> ngeq X (y::Y).
+Proof.
+split.
+{ 
+intros; destruct H; apply forall_ngeq_r;intros;elim H1.
+intros;rewrite <-H2;tauto.
+apply forall_ngeq_r;tauto.
+} {
+intros;rewrite (forall_ngeq_r X (y :: Y)) in H;split.
+rewrite (forall_ngeq_r X Y);intros;apply H;apply in_cons;eauto.
+apply H;apply in_eq.
+}
+Qed.
 
 
 (* TODO mejorar la notacion *)
@@ -166,9 +197,6 @@ clear H;intro H;elim H.
 intros. elim H0.
 Qed.
 
-Definition bad_number(X Y Z: symbol):= leq X Y /\ leq Y Z /\ ~leq X Z.
-
-
 (* ----------------- Lemas auxiliares -----------------*)
 Lemma not_leq_to_or: forall (X Y: symbol), (leq X Y -> False) -> ((ngeq (left X) [Y]-> False) \/ (ngeq [X] (right Y) ->False)).
 Proof.
@@ -193,7 +221,7 @@ Qed.
 
 Lemma not_forall_exists_not_l: forall (x Y: symbol),forall (X: list symbol),
              ((~ forall x : symbol, In x X -> ngeq [x] [Y])) ->
-             ((exists x : symbol, In x X -> (ngeq [x] [Y]->False))).
+             ((exists x : symbol, In x X /\ (ngeq [x] [Y]->False))).
 Proof.
 intros.
 apply not_all_ex_not in H.
@@ -214,7 +242,7 @@ Qed.
 
 Lemma not_forall_exists_not_r: forall (y X: symbol),forall (Y: list symbol),
              ((~ forall y : symbol, In y Y -> ngeq [X] [y])) ->
-               ((exists y : symbol, In y Y -> (ngeq [X] [y]->False))).
+               ((exists y : symbol, In y Y /\ (ngeq [X] [y]->False))).
 Proof.
 intros.
 apply not_all_ex_not in H.
@@ -225,62 +253,56 @@ exists x.
 tauto.
 Qed.
 
+
+Definition bad_number(X Y Z: symbol):= (leq X Y /\ leq Y Z /\ ~leq X Z).
+
 (* ----------------- Lemas auxiliares -----------------*)
 
 Lemma bad_numbers: forall (X Y Z: symbol), bad_number X Y Z ->
-               (exists x : symbol, In x (left X) -> bad_number Y Z x) \/
-               (exists z : symbol, In z (right Z) ->bad_number z X Y).
+               (exists x : symbol, In x (left X) /\ bad_number Y Z x) \/
+               (exists z : symbol, In z (right Z) /\ bad_number z X Y).
 
 Proof.
   unfold bad_number.
-  intros.
-  destruct H.
-  destruct H0.
-  apply not_leq_to_or in H1.
-  destruct H1.
+  intros X Y Z H;
+  destruct H; destruct H0;
+  apply leq_def in H; destruct H;
+  apply leq_def in H0; destruct H0;
+  apply not_leq_to_or in H1; destruct H1.
   {
     left.
-    apply leq_def in H.
-    apply leq_def in H0.
-    destruct H.
-    destruct H0.
     apply not_forall_ngeq_l in H1; auto.
     apply not_forall_exists_not_l in H1 ; auto.
-    elim H1.
-    intros.
+    repeat destruct H1.
     exists x.
-    intros.
+    split.
+    tauto.
     split. { apply leq_def. split;  eauto. }
            { split. { apply ngeq_n. eauto. }
                     { unfold not. apply leq_n. rewrite (forall_ngeq_l (left X) [Y]) in H. eauto. } }
   }
   {
     right.
-    apply leq_def in H.
-    apply leq_def in H0.
-    destruct H.
-    destruct H0.
     apply not_forall_ngeq_r in H1 ; auto.
     apply not_forall_exists_not_r in H1 ; auto.
     elim H1.
     intros z.
     intros.
+    destruct H4.
     exists z.
-    intros.
-    split.  {apply ngeq_n. eauto. }
+    split.
+    tauto.
+    split. 
+     {apply ngeq_n. eauto. }
             { split; [ apply leq_def | unfold not; apply leq_n; rewrite (forall_ngeq_r [Y] (right Z)) in H3]; eauto. } 
   }
 Qed.
-
-Print fold_right.
 
 (**Mas numeros **)
 
 Fixpoint D (s:symbol) (n:nat): nat := 1 + max n (max (fold_right (D) 0 (left s)) (fold_right (D) 0 (right s))).
 
 Definition Dx (l: list symbol) : nat := fold_right (D) 0 l.
-
-
 
 
 Lemma one_number_lower_limit: forall (X: symbol)(n :nat), D X n >=1.
@@ -297,130 +319,75 @@ Qed.
 
 Lemma aux: forall (X: symbol) (n :nat), D X n = max (S n) (D X 0).
 Proof.
-intros.
-induction X.
-induction l;induction l0;simpl;lia.
+intros; induction X; induction l;induction l0;simpl;lia.
 Qed.
 
 Lemma aux3: forall (X: symbol), D X 0 =  max (S(Dx (left X))) (S(Dx (right X))).
 Proof.
-intros.
-induction X.
-induction l;induction l0;tauto.
+intros; induction X; induction l; induction l0; tauto.
 Qed.
 
 Lemma aux4: forall (X: symbol) (n :nat), D X n =  ( max (S(n)) ((( max (S(Dx (left X))) (S(Dx (right X))))))).
 Proof.
-intros.
-induction X.
-induction l;induction l0;induction n;unfold left; unfold right; simpl; tauto.
+intros; induction X; induction l;induction l0;induction n;unfold left; unfold right; simpl; tauto.
 Qed.
 
 Lemma aux5: forall (X: symbol) (n :nat), D X n >= D X 0.
 Proof.
-intros.
-induction n.
+intros; induction n.
 elim one_number_lower_limit; auto.
-rewrite aux4.
-rewrite aux3.
-lia.
+rewrite aux4;rewrite aux3;lia.
 Qed.
 
 Lemma aux6: forall (X: symbol) (n :nat), (D X n) =  (S( max n ( max (Dx (left X)) (Dx (right X))))).
 Proof.
-intros.
-induction X.
-induction l;induction l0;induction n;unfold left; unfold right; simpl; tauto.
-Qed.
-
-Lemma smax: forall (n1 n2: nat), max (S n1) n2 >= max n1 n2.
-Proof.
-intros.
-lia.
+intros; induction X; induction l; induction l0; induction n; unfold left; unfold right; simpl; tauto.
 Qed.
 
 Lemma auxn_l: forall (X x: symbol), In x (left X) -> D X 0 > D x 0.
 Proof.
-intros.
-induction X.
-unfold left in H.
-induction l.
-elim H.
-elim H.
-intros.
-rewrite H0.
-rewrite aux4.
-unfold left.
-simpl.
-elim aux5.
-lia.
-intros.
-lia.
-intros.
-apply IHl in H0.
-rewrite aux6.
-unfold right.
-unfold left.
-simpl.
-rewrite aux.
-rewrite aux6 in H0.
-simpl in H0.
-lia.
+intros;induction X; unfold left in H.
+induction l; elim H; intros.
+rewrite H0; rewrite aux4; unfold left; simpl;elim aux5; intros; lia.
+apply IHl in H0; rewrite aux6; unfold right; unfold left; simpl; rewrite aux; rewrite aux6 in H0; simpl in H0; lia.
 Qed.
 
 Lemma auxn_r: forall (X x: symbol), In x (right X) -> D X 0 > D x 0.
 Proof.
-intros.
-induction X.
-unfold right in H.
-induction l0.
-elim H.
-elim H.
-intros.
-rewrite H0.
-
-rewrite aux4.
-unfold left.
-simpl.
-elim aux5.
-lia.
-intros.
-lia.
-intros.
-apply IHl0 in H0.
-rewrite aux6.
-unfold right.
-unfold left.
-simpl.
-
-rewrite aux.
-
-rewrite aux6 in H0.
-simpl in H0.
-lia.
+intros;induction X; unfold right in H.
+induction l0; elim H; intros.
+rewrite H0; rewrite aux4; unfold left; simpl;elim aux5; intros; lia.
+apply IHl0 in H0; rewrite aux6; unfold right; unfold left; simpl; rewrite aux; rewrite aux6 in H0; simpl in H0; lia.
 Qed.
 
 
-Lemma bad_numbers_dec: forall (X Y Z: symbol) (n :nat), bad_number X Y Z -> D X 0 + D Y 0 + D Z 0= n ->
-               ((exists x : symbol, In x (left X) -> bad_number Y Z x /\ D x 0+ D Y 0+ D Z 0 < n ) \/
-               ((exists z : symbol, In z (right Z) ->bad_number z X Y /\ D z 0+ D Y 0+ D X 0 < n))).
+Lemma bad_numbers_dec: forall (X Y Z: symbol) (n :nat), bad_number X Y Z -> D X 0 + D Y 0 + D Z 0 = n ->
+               ((exists x : symbol, In x (left X)  /\ bad_number Y Z x /\ D x 0 + D Y 0 + D Z 0 < n ) \/
+               ((exists z : symbol, In z (right Z) /\ bad_number z X Y /\ D X 0 + D Y 0 + D z 0 < n))).
+Proof.
+intros; apply bad_numbers in H.
+repeat destruct H.
+left;exists x; split; eauto; split; eauto; apply auxn_l in H; lia.
+right;exists x; split; eauto; split; eauto; apply auxn_r in H; lia.
+Qed.
+
+
+Lemma ex_leq_trans: 
+forall (n: nat) (X Y Z: symbol), bad_number X Y Z -> D X 0 + D Y 0 + D Z 0 < n -> False. 
+Proof.
+intro; induction n; lia||auto; intros; apply (bad_numbers_dec X Y Z (D X 0 + D Y 0 + D Z 0)) in H; auto;
+repeat destruct H;destruct H1; [apply (IHn Y Z x ) | apply (IHn x X Y) ];lia||auto. 
+Qed.
+
+Lemma leq_trans: 
+forall (X Y Z: symbol), leq X Y -> leq Y Z -> leq X Z.
 Proof.
 intros.
-apply bad_numbers in H.
-elim H; intros.
-left.
-elim H1;intros.
-exists x.
-split.
-tauto.
-apply auxn_l in H3.
-lia.
-right.
-elim H1;intros.
-exists x.
-split.
-tauto.
-apply auxn_r in H3.
+elim (classic (X _<=_ Z));eauto.
+intros.
+cut (False);tauto||auto.
+apply (ex_leq_trans ( S (D X 0 + D Y 0 + D Z 0)) X Y Z).
+unfold bad_number; auto.
 lia.
 Qed.
 
